8a7239abdc6cfef71d87413a8464974762090fab
Jeremy Apthorp <nornagon@nornagon.net>
2019-07-13 14:41:36 -0700

    each, grid, add

--- index.ts
+++ index.ts
@@ -3,8 +3,12 @@ import assert from 'assert'
 
 enum Type {
   Scalar,
+  Natural,
   Vec2,
   Function,
+  Vec2Array,
+  ScalarArray,
+  NaturalArray,
 }
 
 interface FunctionDef {
@@ -22,7 +26,7 @@ interface BlockDef {
 }
 
 type Expression = {
-  type: 'scalar'
+  type: 'literal'
   value: number
 } | {
   type: 'call'
@@ -46,6 +50,11 @@ const globalFunctionsList: FunctionDef[] = [{
   argTypes: [Type.Scalar, Type.Scalar],
   outputType: Type.Vec2,
   fn: (x, y) => [x,y]
+}, {
+  name: 'addVecs',
+  argTypes: [Type.Vec2, Type.Vec2],
+  outputType: Type.Vec2,
+  fn: ([x1, y1], [x2, y2]) => [x1 + x2, y1 + y2]
 }]
 const globalFunctions: {[k: string]: FunctionDef} = {}
 globalFunctionsList.forEach(f => {globalFunctions[f.name] = f})
@@ -63,6 +72,24 @@ const globalBlocksList: BlockDef[] = [{
     const [start, end] = args
     console.log('line from', start, end)
   },
+}, {
+  name: 'grid',
+  argTypes: [Type.Natural, Type.Natural],
+  fn: (args, scope, bindingNames, contents) => {
+    const [nx, ny] = args
+    const points: [number, number][] = []
+    for (let y = 0; y < ny; y++) for (let x = 0; x < nx; x++) points.push([x/(nx === 1 ? 1 : nx-1),y/(ny === 1 ? 1 : ny-1)])
+    queueBlockContents(subScope(scope, {[bindingNames[0]]: points}), contents)
+  }
+}, {
+  name: 'each',
+  argTypes: [Type.Vec2Array],
+  fn: (args, scope, bindingNames, contents) => {
+    const [arr] = args
+    for (const a of arr) {
+      queueBlockContents(subScope(scope, {[bindingNames[0]]: a}), contents)
+    }
+  }
 }]
 const globalBlocks: {[k: string]: BlockDef} = {}
 globalBlocksList.forEach(f => {globalBlocks[f.name] = f})
@@ -71,7 +98,7 @@ globalBlocksList.forEach(f => {globalBlocks[f.name] = f})
 type Scope = {[k:string]: any}
 const evaluate = (scope: Scope, expr: Expression): any => {
   switch (expr.type) {
-    case 'scalar': return expr.value
+    case 'literal': return expr.value
     case 'variable': {
       const val = scope[expr.name]
       assert(val != null)
@@ -83,7 +110,6 @@ const evaluate = (scope: Scope, expr: Expression): any => {
       const args = expr.args.map(arg => evaluate(scope, arg))
       return callee.fn(...args)
     }
-
   }
 }
 
@@ -111,32 +137,63 @@ const coolprog: Block = {
   name: '_get_hyp',
   bindingNames: ['t'],
   args: [],
-  contents: [{
-    name: 'line',
-    bindingNames: [],
-    args: [{
-      type: 'call',
-      callee: {
-        type: 'variable',
-        name: 'Vec',
-      },
-      args: [
-        {type: 'scalar', value: 0},
-        {type: 'scalar', value: 1},
+  contents: [
+    {
+      name: 'grid',
+      args: [{
+        type: 'literal',
+        value: 2
+      }, {
+        type: 'literal',
+        value: 2
+      }],
+      bindingNames: ['g'],
+      contents: [
+        {
+          name: 'each',
+          args: [{
+            type: 'variable',
+            name: 'g',
+          }],
+          bindingNames: ['p'],
+          contents: [
+            {
+              name: 'line',
+              bindingNames: [],
+              args: [{
+                type: 'variable',
+                name: 'p'
+              }, {
+                type: 'call',
+                callee: {
+                  type: 'variable',
+                  name: 'addVecs',
+                },
+                args: [
+                  {
+                    type: 'variable',
+                    name: 'p'
+                  },
+                  {
+                    type: 'call',
+                    callee: {
+                      type: 'variable',
+                      name: 'Vec',
+                    },
+                    args: [
+                      { type: 'literal', value: 1 },
+                      { type: 'literal', value: 0 },
+                    ]
+                  }
+                ]
+              }],
+              contents: []
+            }
+          ]
+        }
       ]
-    }, {
-      type: 'call',
-      callee: {
-        type: 'variable',
-        name: 'Vec',
-      },
-      args: [
-        {type: 'scalar', value: 1},
-        {type: 'scalar', value: 0},
-      ]
-    }],
-    contents: []
-  }]
+    },
+  ]
 }
 
 run(coolprog)
